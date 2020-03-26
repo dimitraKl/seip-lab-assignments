c346c44db47aa8164085f5822f7cfa7797be95f6
Dimitra K <dxkgml@gmail.com>
2020-03-26 05:41:33 +0200

    add makeHistogram method

--- seip2020_practical_assignments/gradeshistogram/src/main/java/gr/aueb/dmst/dimitrakallini/HistogramGenerator.java
+++ seip2020_practical_assignments/gradeshistogram/src/main/java/gr/aueb/dmst/dimitrakallini/HistogramGenerator.java
@@ -5,6 +5,12 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class HistogramGenerator {
 
@@ -30,12 +36,57 @@ public class HistogramGenerator {
 		
 		return grades;
 	}
+
+
+	public void makeHistogram(int[] grades) {
+		/*
+		 * The XYSeriesCollection object is a set XYSeries series (dataset) that
+		 * can be visualized in the same chart
+		 */
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		/*
+		 * The XYSeries that are loaded in the dataset. There might be many
+		 * series in one dataset.
+		 */
+		XYSeries data = new XYSeries("random values");
+
+		/*
+		 * Populating the XYSeries data object from the input Integer array
+		 * values.
+		 */
+		for (int i = 0; i < grades.length; i++) {
+			data.add(i, grades[i]);
+		}
+
+		// add the series to the dataset
+		dataset.addSeries(data);
+
+		boolean legend = false; // do not visualize a legend
+		boolean tooltips = false; // do not visualize tooltips
+		boolean urls = false; // do not visualize urls
+
+		// Declare and initialize a createXYLineChart JFreeChart
+		JFreeChart chart = ChartFactory.createXYLineChart("Chart title", "x_axis title", "y_axis_title", dataset,
+				PlotOrientation.VERTICAL, legend, tooltips, urls);
+
+		/*
+		 * Initialize a frame for visualizing the chart and attach the
+		 * previously created chart.
+		 */
+		ChartFrame frame = new ChartFrame("First", chart);
+		frame.pack();
+		// makes the previously created frame visible
+		frame.setVisible(true);
+
+	}
+
 	
 	public static void main(String[] args) {
 		String filename = args[0];
 		HistogramGenerator hm = new HistogramGenerator();
 		
 		int[] grades = hm.readFromFile(filename);
+		hm.makeHistogram(grades);
 
 	}
 
