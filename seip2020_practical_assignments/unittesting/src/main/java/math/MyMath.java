6e8f0296c8072427f92730abd74f37decc11ae2c
Dimitra K <dxkgml@gmail.com>
2020-04-19 23:57:54 +0300

    add javadoc to MyMath.java

--- seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
@@ -1,7 +1,21 @@
 package math;
 
+/**
+* The MyMath provides simple math operations
+* that serve as hands-on practice on Unit Testing.
+*
+* @author  dimitrakallini
+* @version 1.0
+* @since   2020-04-19
+*/
 public class MyMath {
 
+	/**
+	 * Calculates and returns the factorial of a given number.
+	 * @param n the input number
+	 * @return the factorial of the given number n
+	 * @exception IllegalArgumentException when n is less than zero or greater than 12
+	 */
 	public int factorial (int n) {
 		if (n < 0 || n > 12) {
 			throw new IllegalArgumentException("n should be > 0 and < 12");
