f915b71a8bca0952df7e165730ba498541be9c87
Dimitra K <dxkgml@gmail.com>
2020-04-20 00:59:38 +0300

    add MyMathTestSuite to group MyMathTest and FactorialParameterizedTest

---
+++ seip2020_practical_assignments/unittesting/src/test/java/math/MyMathTestSuite.java
@@ -0,0 +1,14 @@
+package math;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   MyMathTest.class,
+   FactorialParameterizedTest.class
+})
+public class MyMathTestSuite {
+
+}
