5fa9fed683b93da1dba8f6a0babb404680241c6a
Dimitra K <dxkgml@gmail.com>
2020-04-21 20:04:15 +0300

    add test cases for isPrime method

--- seip2020_practical_assignments/unittesting/src/test/java/math/MyMathTest.java
+++ seip2020_practical_assignments/unittesting/src/test/java/math/MyMathTest.java
@@ -1,5 +1,6 @@
 package math;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MyMathTest {
@@ -13,6 +14,21 @@ public class MyMathTest {
 	@Test (expected = IllegalArgumentException.class)
 	public void testFactorialAboveTwelveException() {
 		mm.factorial(15);
+	}
+
+	@Test
+	public void testIsPrimeTrue() {
+		Assert.assertEquals(true, mm.isPrime(11));
+	}
+
+	@Test
+	public void testIsPrimeFalse() {
+		Assert.assertEquals(false, mm.isPrime(15));
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void testIsPrimeNumberBelowTwoException() {
+		mm.factorial(-3);
 	}
 
 }
