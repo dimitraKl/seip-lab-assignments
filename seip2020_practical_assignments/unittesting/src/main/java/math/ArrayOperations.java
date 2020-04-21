905652f69f58c619eae97368a8ca7ca06fabc5fb
Dimitra K <dxkgml@gmail.com>
2020-04-21 20:23:30 +0300

    add javadoc to class ArrayOperations

--- seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
@@ -3,15 +3,29 @@ package math;
 import io.FileIO;
 import java.util.ArrayList;
 
+/**
+* The ArrayOperations provides simple array operations
+* that serve as hands-on practice on Unit Testing.
+*
+* @author  dimitrakallini
+* @version 1.0
+* @since   2020-04-21
+*/
 public class ArrayOperations {
 
+	/**
+	 * Reads numbers from a file and returns an array with the prime numbers.
+	 * @param fileIo the object of FileIO
+	 * @param filepath the input file
+	 * @param myMath the object of MyMath
+	 * @return the integer array with the prime numbers
+	 */
 	public int[] findPrimesInFile(FileIO fileIo, String filepath, MyMath myMath) {
 		int[] array = fileIo.readFile(filepath);
 		ArrayList<Integer> al = new ArrayList<Integer>();
-		MyMath mm = new MyMath();
 
 		for (int a : array) {
-			if (mm.isPrime(a))
+			if (myMath.isPrime(a))
 				al.add(a);
 		}
 		
