cdc069d99b9a12f0f3af44ffca0edc13d3259d97
Dimitra K <dxkgml@gmail.com>
2020-04-21 21:28:04 +0300

    add mocks for FileIO and MyMath instances in test case for findPrimesInFile method

--- seip2020_practical_assignments/unittesting/src/test/java/math/ArrayOperationsTest.java
+++ seip2020_practical_assignments/unittesting/src/test/java/math/ArrayOperationsTest.java
@@ -1,25 +1,36 @@
 package math;
 
 import java.io.File;
+import io.FileIO;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import io.FileIO;
+import static org.mockito.Mockito.*;
 
 public class ArrayOperationsTest {
 	ArrayOperations ao = new ArrayOperations();
-	FileIO fio = new FileIO();
-	MyMath mm = new MyMath();
 
 	@Test
 	public void testFindPrimesInFile() {
-		int[] myArray = new int[]{2,29,53,17,97};
+		//Mock the dependencies
+		FileIO fio = mock(FileIO.class);
+		MyMath mm = mock(MyMath.class);
+
+		int[] myArray = new int[]{2,29,53};
 		
 		String resourceName = "testFilePrimeNumbers.txt";
 		ClassLoader classLoader = getClass().getClassLoader();
 		File file = new File(classLoader.getResource(resourceName).getFile());
 		String absolutePath = file.getAbsolutePath();
+
+		int[] fileResultValues = new int[]{2,27,88,29,53};
+		when(fio.readFile(absolutePath)).thenReturn(fileResultValues);
+
+		when(mm.isPrime(2)).thenReturn(true);
+		when(mm.isPrime(27)).thenReturn(false);
+		when(mm.isPrime(88)).thenReturn(false);
+		when(mm.isPrime(29)).thenReturn(true);
+		when(mm.isPrime(53)).thenReturn(true);
 
 		Assert.assertArrayEquals(myArray, ao.findPrimesInFile(fio, absolutePath, mm));
 	}
