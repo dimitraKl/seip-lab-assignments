62b9e5df0089bb3e27f22a853e2eab6e5ab242b0
Dimitra K <dxkgml@gmail.com>
2020-05-31 19:01:47 +0300

    add FileExporterFactory to handle the different file exporters

---
+++ seip2020_practical_assignments/SourceCodeAnalyzer/src/main/java/codeanalyzer/FileExporterFactory.java
@@ -0,0 +1,26 @@
+package codeanalyzer;
+
+/**
+ * Creates the appropriate exporter based on the given output
+ * file type in order to export the metrics. 
+ * The output can be CSV or JSON files, otherwise it will lead
+ * to unknown output file type error.
+ * @author dimitrakallini
+ */
+public class FileExporterFactory {
+	
+	public FileExporter createFileExporter(String outputType) {
+
+		FileExporter exporter;
+		if (outputType.equals("csv")) {
+			exporter = new CsvExporter();
+		} else if (outputType.equals("json")) {
+			exporter = new JsonExporter();
+		} else {
+			exporter = new NullExporter(outputType);
+		}
+
+		return exporter;
+	}
+
+}
